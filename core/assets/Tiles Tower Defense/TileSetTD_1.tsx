f7b98648fd4c4177f24b629f6953d13c1d9f325c
Malakyas <afa7789@gmail.com>
2017-10-23 10:42:17 -0200

    tentando ainda
    mudando algo, perdi o q tinha feito q era pouca coisa e não tinha upado

---
+++ core/assets/Tiles Tower Defense/TileSetTD_1.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TileSetTD" tilewidth="32" tileheight="32" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="tile 1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="tile 2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="tile 3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="tile 4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="tile 5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="tile 6.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="tile 7.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="tile 8.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="tile 9.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="tile 10.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="tile 11.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="tile 12.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="tile 13.png"/>
+ </tile>
+</tileset>
