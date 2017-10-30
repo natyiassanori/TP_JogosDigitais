f3fced5ac1410f0a552ded6cc47f9a0439fe99ff
Malakyas <afa7789@gmail.com>
2017-10-29 23:33:52 -0200

    Noss hj consegui fazer nada
    PQP, então neh, tem q por pra quando chegar, o attack desaparecer, seila tirar dum ARrayList e o Enemy tomar o dano propricio.

---
+++ core/src/br/cefetmg/games/Enemy.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cefetmg.games;
+
+import br.cefetmg.games.movement.Position;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ *
+ * @author Dota
+ */
+public class Enemy {
+        public Vector2 position;
+}
